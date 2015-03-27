adb791337eff92ff61846f7c2adc83a873151a4c
Renette Ros <renetteros@gmail.com>
2015-03-27 09:16:35 +0200

    Added Content's views to routing, update main package

--- Handlebars Server/routes/index.js
+++ Handlebars Server/routes/index.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+
+var bodyparser = require('body-parser');
+var urlencodedparser = bodyparser.urlencoded({extended:false});
+var resources = require('resources');
+var reports = require('reporting');
+var formidable = require('formidable');
+
 function getProfile (id) {
  return {title: "user " + id};
 }
@@ -21,6 +28,117 @@ router.get('/testing', function(req, res, next) {
   res.render('test', getProfile(req.query.id));
 });
 
+//************************ CONTENT ****************************//
+
+// loads test page for DEMO
+router.get('/testPost', function(req, res, next){
+
+    res.render('dynamic_views/postInput', {
+        success : ""
+    });
+});
+
+// loads manage constraints page for DEMO
+router.get('/manageConstraints', function(req, res, next){
+
+    resources.getConstraints(function(err, results){
+        if (err){
+            var html = "<p>Could not retrieve requested information from the database.</p>"
+        } else {
+            html = "<table><tr><td>Mime Type</td><td>File Size Limit</td></tr>\r\n";
+            results.forEach(function(con){
+                html += "<tr><td>"+con.mime_type+
+                "</td><td>"+Math.round(con.size_limit/(1024*1024))+
+                " MB</td><td><a href='/removeConstraint/"+con._id+"'>remove</a></td></tr>\r\n";
+            });
+            html += "</table>";
+            res.render('dynamic_views/constraintsManagement', { results:  html});
+        }
+    });
+});
+
+// add constraint page for DEMO
+router.get('/addConstraint', function(req, res, next){
+
+    res.render('dynamic_views/addConstraint', {
+        success : ""
+    });
+});
+
+// remove constraint from database
+router.get('/removeConstraint/*', function(req, res, next){
+
+    var id = req.url.replace('/removeConstraint/', "");
+    resources.removeConstraint(id, function(truth){
+        if (!truth){
+                res.render('dynamic_views/constraintsManagement', {
+                    results: "An error occurred!"
+                });
+        } else {
+            resources.getConstraints(function(err, results){
+                if (err){
+                    var html = "<p>Could not retrieve requested information from the database.</p>";
+                } else {
+                    html = "<table><tr><td>Mime Type</td><td>File Size Limit</td></tr>\r\n";
+                    results.forEach(function(con){
+                        html += "<tr><td>"+con.mime_type+
+                        "</td><td>"+Math.round(con.size_limit/(1024*1024))+
+                        " MB</td><td><a href='/removeConstraint/"+con._id+"'>remove</a></td></tr>\r\n";
+                    });
+                    html += "</table>";
+                    res.render('dynamic_views/constraintsManagement', { results:  html});
+                }
+            });
+        }
+    });
+});
+
+// adds a new constraint (for DEMO)
+router.post('/submitConstraint', urlencodedparser, function(req, res, next){
+
+    resources.addConstraint(req.body.mime_type, req.body.size_limit, function(truth){
+        if (!truth){
+            res.render('dynamic_views/constraintsManagement', {
+                results: "An error occurred!"
+            });
+        } else {
+            resources.getConstraints(function(err, results){
+                if (err){
+                    var html = "<p>Could not retrieve requested information from the database.</p>"
+                } else {
+                    html = "<table><tr><td>Mime Type</td><td>File Size Limit</td></tr>\r\n";
+                    results.forEach(function(con){
+                        html += "<tr><td>"+con.mime_type+
+                        "</td><td>"+Math.round(con.size_limit/(1024*1024))+
+                        " MB</td><td><a href='/removeConstraint/"+con._id+"'>remove</a></td></tr>\r\n";
+                    });
+                    html += "</table>";
+                    res.render('dynamic_views/constraintsManagement', { results:  html});
+                }
+            });
+        }
+    });
+});
+
+// upload a new file (for DEMO)
+router.post('/submitPost', [ function(req, res, next){
+
+    //var form = new formidable.IncomingForm();
+    //
+    //form.parse(req, function(err, fields, files){
+    //
+    //    // create post object
+    //    var id = 0; // get postID
+        resources.uploadFile(req, res, next, 0);
+    //});
+}, function(req, res, next){
+    res.render('dynamic_views/postInput', {
+        success : res.fileState
+    });
+}]);
+
+
+//****************** INFRASTRUCURE *******************************//
 
 
 module.exports = router;
