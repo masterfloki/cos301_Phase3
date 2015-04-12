244bbf65fcde246bc21bd22b08c0b356239c6fd3
Szymon Ziolkowski <nomyzs.z@gmail.com>
2015-04-12 17:16:01 +0200

    settings was a bit buggy

--- Handlebars Server/app.js
+++ Handlebars Server/app.js
@@ -19,7 +19,7 @@ IoC.loader(IoC.node( __dirname + "/node_modules"));
  */
 app.interceptor = IoC.create('../interceptor/interceptor.js', app);
 app.routes = IoC.create('../routes', app);
-app.settings = IoC.create('buzz-settings', app);
+//app.settings = IoC.create('buzz-settings', app);
 app.commands = IoC.create('buzz-commands', app);
 
 app.use(scribe.express.logger()); //Log each request
